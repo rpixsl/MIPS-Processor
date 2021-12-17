5ad1f9b4412c72fdc9ccb91b0c239ed39082698c
Tianrui Qi <74130971+tianrui-qi@users.noreply.github.com>
2021-12-17 00:27:47 -0500

    Delete Pycharm directory

--- Pycharm/plot.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from matplotlib import pyplot as plt
-
-if __name__ == "__main__":
-
-    fig, axs = plt.subplots(1)
-    axs.plot( np.loadtxt("Train_Loss.txt"), label="01" )
-    plt.show()
-
